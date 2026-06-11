d4a3d553d95c34f563dd2b5f13c6b21a4c9a9012
Juan Manuel <a01639125@tec.mx>
2026-06-10 19:52:14 -0600

    Add teams fetching

--- MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/MyTodoListApplication.java
+++ MtdrSpring/backend/src/main/java/com/springboot/MyTodoList/MyTodoListApplication.java
@@ -2,11 +2,8 @@ package com.springboot.MyTodoList;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    RabbitAutoConfiguration.class
-})
+@SpringBootApplication
 public class MyTodoListApplication {
 
 	public static void main(String[] args) {
