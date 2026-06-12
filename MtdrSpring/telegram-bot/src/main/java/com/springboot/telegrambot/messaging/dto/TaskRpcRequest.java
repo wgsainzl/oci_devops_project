c59a8001d340dd8883358625261a8787eefa4e45
Diego Rodríguez <69479687+DiegoIvanR@users.noreply.github.com>
2026-06-11 22:10:47 -0600

    another small fix

--- MtdrSpring/telegram-bot/src/main/java/com/springboot/telegrambot/messaging/dto/TaskRpcRequest.java
+++ MtdrSpring/telegram-bot/src/main/java/com/springboot/telegrambot/messaging/dto/TaskRpcRequest.java
@@ -35,9 +35,6 @@ public class TaskRpcRequest implements Serializable {
     public double getActualHours() {return actualHours; }
     public void setActualHours(double actualHours) { this.actualHours = actualHours; }
 
-    public double getActualHours() {return actualHours; }
-    public void setActualHours(double actualHours) { this.actualHours = actualHours; }
-
     public String getTelegramId() { return telegramId; }
     public void setTelegramId(String telegramId) { this.telegramId = telegramId; }
     
